301dbbf660d6dd3fb53e24272c2e93766ad276b1
Draply <doduc0610@gmail.com>
2023-02-10 10:56:49 +0700

    update

--- nft-marketplace-version2/contracts/NFT.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.9;
-import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-contract NFT is ERC721URIStorage, ERC721Enumerable  {
-    constructor
-
-}
