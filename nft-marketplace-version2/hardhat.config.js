a842ee759456d501abb8293a0a62800548c66d08
Draply <doduc0610@gmail.com>
2023-02-14 14:13:38 +0700

    .

--- nft-marketplace-version2/hardhat.config.js
+++ nft-marketplace-version2/hardhat.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     goerli: {
       url:"" ,
-      accounts: ""
+      accounts: "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
     }
   },
   solidity: {
